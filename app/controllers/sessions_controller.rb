5772b975bf93ba38f0875faee0f053d1eb23557d
isodadosi <isodadosi@gmail.com>
2020-02-28 17:33:46 +0900

    ユーザー機能、ログインなどの仮追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+    
+  end
+
   def create
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to users_path
+    else
+      redirect_to new_session_path, flash: {
+        notice: "一致するものが見当たりませんでした"
+      }
+    end
   end
 
   def destroy
+    session.delete(:user_id)
+    redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ApplicationRecord
+
+    has_secure_password
+
+    validates :name,
+        presence: true,
+        uniqueness: true,
+        length: { maximum: 16 }
+    validates :password,
+        length: { minimum: 4 }
+
+    def self.search(search)
+        if search
+            where(['name LIKE ?', "%#{search}%"]) 
+        else
+            all
+        end
+    end
+    
 end
