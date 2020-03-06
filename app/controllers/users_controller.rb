7f10eb773fa8f2e1b49f861bed807bfb463fa6b2
isodadosi <isodadosi@gmail.com>
2020-03-07 00:09:02 +0900

    簡単な管理者機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,22 @@ class UsersController < ApplicationController
 
   def index
     if @current_user
+      if @current_user.name == "admin"
+        @contents = Content.page(params[:page])
+        @shops = Shop.page(params[:page])
+      else
+        search_name = @current_user.name
+        @contents = Content.search_my_content(search_name).page(params[:page])
+        @shops = Shop.search_my_content(search_name).page(params[:page])
+      end
       search_name = @current_user.name
     else
       search_name = "ゲスト"
     end
-    @contents = Content.search_my_content(search_name).page(params[:page])
-    @shops = Shop.search_my_content(search_name).page(params[:page])
+    
   end
+
+  
 
   def new
     @user = User.new(flash[:user])
